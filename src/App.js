56dd0888e59ef4014b93eb7d665f1f6c2f96b2d5
nidheeshmd <md.nidheesh3@gmail.com>
2020-07-01 16:02:12 +0200

    basement

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import HomePage from './homepage/homepage.component';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+<HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
