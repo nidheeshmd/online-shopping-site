8a13137689df4414fcd4fff10aece2aa46cd99f8
nidheeshmd <md.nidheesh3@gmail.com>
2020-07-02 20:29:24 +0200

    work progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
 function App() {
   return (
     <div>
-<HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/shop' component={ShopPage}></Route>
+      </Switch>
     </div>
   );
+  /*
+  <Route exact path='/' component={HomePage}/>
+  <Route path='/hats' component={HatsPage}/>
+
+  if property path property value is wrong, router will show unwanted pages
+  so we put exact propery as true, so any wrong spelling in the address it will not show
+  any other pages. In other words show 404 page
+
+  <Switch></Switch>
+
+  the moment switch find the match, it show that page and omit other pages
+  for example path='/' & path='/hats' this will show the home page insted of 
+  hats page(must omit exact property). allows us 404 page.
+  */
+
 }
 
 export default App;
--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { withRouter} from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, id, imageUrl, size }) => (
-    <div className={`${size} menu-item`}
-        key={id}>
+const MenuItem = ({ title, id, imageUrl, size, history, linkUrl, match}) => (
+    <div className={`${size} menu-item`} 
+    key={id}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image'
             style={{ backgroundImage: `URL(${imageUrl})` }}>
         </div>
@@ -15,4 +17,7 @@ const MenuItem = ({ title, id, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
+
+/*with 'withRouter' we get the access of 3 properties of react router
+location, history  and history*/
