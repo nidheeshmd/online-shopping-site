1e765b0286d30a8b2b8af672c560a88d6ad5adc4
nidheeshmd <md.nidheesh3@gmail.com>
2020-07-12 13:01:42 +0200

    added redux persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';// object of browser storeage
+
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root', //key start storing elements from root to above
+    storage,//which storage is using, we import browser storage above
+    whitelist: ['cart']  //array of string names that we needed to store in our storage
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig,rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 //this store is not related with online shop, this is the store of redux cycle.
 import { createStore, applyMiddleware} from 'redux';
+import { persistStore} from 'redux-persist';// used to store our items to localstorage , so items will not remove when we refresh our page
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default (store , persistor);
